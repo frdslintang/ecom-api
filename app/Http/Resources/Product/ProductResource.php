9d7521833b6af6a4ce2ad48c57f4a7ad8a8a48ff
frdslintang <lintangfirdaus2853@gmail.com>
2024-04-12 22:41:22 +0700

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round($this->price * (100 - $this->discount) / 100, 2),
+            //contoh : 'totalPrice' => round(20000 * (100 - 15) / 100, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this
+            ->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
